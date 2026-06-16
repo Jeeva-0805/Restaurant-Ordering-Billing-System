8a933d08c6debb7040984f39c532d987cd011294
Jeeva-0805 <jeevamass0805@gmail.com>
2026-06-16 15:01:34 +0530

    Initialize database and create user table

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,13 @@
 from app import create_app
+from extensions import db
+from models.user import User  
+
+
+app = create_app()
+with app.app_context():
+    db.create_all()  # Create tables if they don't exist
+
+
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
