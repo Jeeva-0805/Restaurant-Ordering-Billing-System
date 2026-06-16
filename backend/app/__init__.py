eb53b7e2b6d8014b33d25accce7a4bc2e5fffe68
Jeeva-0805 <jeevamass0805@gmail.com>
2026-06-16 15:26:03 +0530

    Configure JWT authentication

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
-from extensions import db
+from extensions import db,jwt
+
 def create_app():
     app=Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    jwt.init_app(app)
     @app.route('/')
     def home():
         return 'Welcome to the Restaurant Ordering and Billing System!'
