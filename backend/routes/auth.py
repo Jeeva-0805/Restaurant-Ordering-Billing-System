8354d0e7e7c22a19883fe7aecdea37b6d8006418
Jeeva-0805 <jeevamass0805@gmail.com>
2026-06-16 17:20:00 +0530

    Implement JWT authentication and protected routes

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from models.user import User
 from extensions import db
+from flask_jwt_extended import (create_access_token,jwt_required,get_jwt_identity)
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -24,7 +25,7 @@ def register():
     if existing_user:
         return {"message": "User already exists"}, 400
     
-    user=User(
+    user = User(
         name=name,
         email=email,
         password=password,
@@ -32,3 +33,41 @@ def register():
     db.session.add(user)
     db.session.commit()
     return {"message": "User registered successfully"}, 201
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+
+    data = request.get_json()
+    
+    if not data:
+        return {"message": "No data provided"}, 400
+
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return {"message": "Email and password are required"}, 400
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return {"message": "Invalid credentials"}, 401
+
+    if user.password != password:
+        return {"message": "Invalid credentials"}, 401
+
+    access_token = create_access_token(identity=str(user.id))
+
+    return {
+        "access_token": access_token
+    }, 200
+
+@auth_bp.route("/profile", methods=["GET"])
+@jwt_required()
+def profile():
+    user_id = get_jwt_identity()
+    return{
+        "message": "Protected route accessed",
+        "user_id": user_id
+    }
