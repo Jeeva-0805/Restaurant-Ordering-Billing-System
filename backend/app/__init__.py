35cea8243b19bc04f978c8db3f2c465fc60a1fdd
Jeeva-0805 <jeevamass0805@gmail.com>
2026-06-16 14:57:08 +0530

    Create user model

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from config import Config
-
+from extensions import db
 def create_app():
     app=Flask(__name__)
     app.config.from_object(Config)
-
+    db.init_app(app)
     @app.route('/')
     def home():
         return 'Welcome to the Restaurant Ordering and Billing System!'
--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,4 @@
 class Config:
     SECRET_KEY = "restaurant-secret-key"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///restaurant.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
